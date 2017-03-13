674e104628a7115cb6ddc5539d86ab7b73f3a79a
Zhen Liu <liuzhen@touchjet.com>
2017-03-13 20:46:50 +1300

    Initial code check in.

---
+++ PiDashcam/PiDashcam/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PiDashcam
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("PiDashcam started");
+			var stillCam = new StillCam();
+			while (Console.ReadKey().KeyChar != 'q')
+			{
+			}
+			stillCam.Stop();
+		}
+	}
+}
---
+++ PiDashcam/PiDashcam/StillCam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Timers;
+using Shell.Execute;
+
+namespace PiDashcam
+{
+	public class StillCam
+	{
+		Timer timer;
+		int imgcounter;
+
+		public StillCam()
+		{
+			timer = new Timer(10000);
+			timer.Elapsed += Timer_Elapsed;
+			timer.Start();
+			imgcounter = 1;
+		}
+
+		public void Stop()
+		{
+			timer.Stop();
+		}
+
+		void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			ProgramLauncher.Execute("raspistill", String.Format("-o {0}.jpg",imgcounter++));
+		}
+	}
+}
